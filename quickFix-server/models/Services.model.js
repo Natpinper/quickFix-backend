82ca9a10f3ad38dbdb4f361bf76139dc58253c4c
Natalia Pinto <pinto.natalia@outlook.com>
2024-04-18 18:18:28 +0200

    enum added to Service Model for categories and subcategories

--- quickFix-server/models/Services.model.js
+++ quickFix-server/models/Services.model.js
@@ -4,10 +4,12 @@ const { Schema, model } = mongoose;
 const servicesSchema = new Schema({
   category: {
     type: String,
+    enum: ["Childcare", "Pets", "Property & Maintenance", "Health & Beauty", "Motoring", "Tuition & Classes"],
     required: [true, "Select a category."],
   },
   subcategory: {
     type: String,
+    enum:["Nannies", "Babysitting", "Petsitting", "Training", "Vets", "Cleaners", "Housekeepers", "Interior Design", "TV & Internet connection","Plumber", "Carpenter","Electrician", "Rubbish removal", "House renovation", "Gardener", "Painter", "Massages", "Hairdressing", "Beauty treatments", "Personal trainer", "Make Up Artist", "Nail care", "Nutrition", "Car repair and Mechanical services", "Car wash", "MOT Testing", "Language", "Music", "Driving lessons", "Academic"],
     required: [true, "Select a category."],
   },
   posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
